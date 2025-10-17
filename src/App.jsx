7a3d876a064e72622ffec87f6f3b3f2e85b9d7cd
CM Mubeen <cmmubeen@gmail.com>
2025-10-17 19:59:10 +0500

    basic signup form validation using react-form-hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import HomePage from "./pages/HomePage/HomePage"
 import SignUp from "./pages/SignUp/SignUp"
+import toast, { Toaster } from "react-hot-toast"
 
 function App() {
 
   return (
     <>
-    <HomePage />
-      {/* <SignUp /> */}
+      <SignUp />
+      {/* <HomePage /> */}
+      <Toaster position="top-right" />
     </>
   )
 }
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,17 +1,62 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import toast from 'react-hot-toast';
+import { useForm } from 'react-hook-form';
+import js from '@eslint/js';
 
 function SignUp() {
+    // ============================ State Management ===========================
+
+    // sign up or sign mode
     const [mode, setMode] = useState("sign up");
 
+    // password visibility
+    const [isVisible, setIsVisible] = useState(false)
+
+    // ============================ react-hook-form setup ===========================
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm({
+        defaultValues: {
+            name: "",
+            username: "",
+            email: "",
+            password: "",
+            terms: false,
+        }
+    })
+
+    // ============================ state handlinng functions ===========================
+
+    // handle to switch between sign up to sign in and vise versa
     const handleSignupToSingin = () => {
         setMode(prev => prev === "sign up" ? "sign in" : "sign up");
     };
 
+    // handle form validation
+    const onSubmit = (data) => {
+        console.log(data)
+        // store data to local storage for autehentication
+        localStorage.setItem("signup", JSON.stringify(data))
+
+        toast.success("🎉 Account created! Please sign in.");
+        reset()
+        setMode("signin")
+    }
+
+
+    // handle isVisible icon
+    const handleIsVisible = () => {
+        setIsVisible(!isVisible)
+    }
+
     return (
         <div className="w-full min-h-screen flex flex-col min-[1149px]:flex-row max-[1149px]:items-center justify-center bg-gradient-to-r from-[#F6F1EB] via-[#EDE7E0] to-[#F6F1EB]">
             {/* Image and logo container */}
-            <div className="image-container w-full max-w-[600px] relative">
+            <div className="image-container w-full max-w-[600px] min-h-[600px] relative">
                 <img src="/imgs/signup-sofa.png" alt="sofa image" className="w-full max-w-[600px] min-[1149px]:max-h-screen object-cover" />
                 <h1 className="absolute top-8 min-[1149px]:top-5 inset-x-0 flex justify-center text-[40px] min-[1149px]:text-[24px] font-[500] font-poppins">
                     Wood Haven
@@ -21,6 +66,7 @@ function SignUp() {
             {/* Signup/Signin form */}
             <div className="login bg-white w-full max-w-[600px] min-h-[600px] flex flex-col justify-center gap-[32px] px-10 min-[1149px]:pl-16 min-[1149px]:pr-24 font-poppins">
 
+                {/* Sign up or Sign In  */}
                 <AnimatePresence mode="wait">
                     <motion.h2
                         key={mode + "-title"}
@@ -34,6 +80,7 @@ function SignUp() {
                     </motion.h2>
                 </AnimatePresence>
 
+                {/* already have an account? or Don't have an account? */}
                 <AnimatePresence mode="wait">
                     <motion.p
                         key={mode + "-switch-text"}
@@ -44,6 +91,8 @@ function SignUp() {
                         className="text-[#6C7275] text-sm min-[1149px]:text-base"
                     >
                         {mode === "sign up" ? "Already have an account? " : "Don't have an account? "}
+
+                        {/* sign up or sign switch button */}
                         <button
                             onClick={handleSignupToSingin}
                             className="text-[#38CB89] font-semibold cursor-pointer transition-transform duration-300 hover:scale-105 hover:text-[#2ea76d]"
@@ -53,8 +102,10 @@ function SignUp() {
                     </motion.p>
                 </AnimatePresence>
 
+                {/* input fields like name, username, email, password */}
                 <AnimatePresence mode="wait">
-                    <motion.div
+                    <motion.form
+                        onSubmit={handleSubmit(onSubmit)}
                         key={mode}
                         initial={{ opacity: 0, y: 5 }} // small shift only
                         animate={{ opacity: 1, y: 0 }}
@@ -63,52 +114,106 @@ function SignUp() {
                         className="flex flex-col gap-4"
                     >
                         {mode === "sign up" && (
-                            <input
-                                type="text"
-                                placeholder="Your name"
-                                className="w-full p-2 mt-2 border-b border-b-gray-300 text-[#6C7275] focus:outline-none text-sm min-[1149px]:text-base"
-                            />
+                            <>
+                                <input
+                                    type="text"
+                                    name='name'
+                                    placeholder="Your name"
+                                    {...register("name", {
+                                        required: "Name is required",
+                                        minLength: { value: 3, message: "Minimum 3 characters" }
+                                    })}
+                                    className="w-full p-2 mt-2 border-b border-b-gray-300 text-[#6C7275] focus:outline-none text-sm min-[1149px]:text-base"
+                                />
+                                {errors.name && (
+                                    <p className="text-red-500 text-xs">{errors.name.message}</p>
+                                )}
+                            </>
                         )}
 
                         <input
                             type="text"
+                            name='username'
                             placeholder="Username"
+                            {...register("username", {
+                                required: "Username is required",
+                                minLength: { value: 3, message: "Minimum 3 characters" }
+                            })}
                             className="w-full p-2 mt-2 border-b border-b-gray-300 text-[#6C7275] focus:outline-none text-sm min-[1149px]:text-base"
                         />
+                        {errors.username && (
+                            <p className='text-red-500 text-xs'>{errors.username.message}</p>
+                        )}
 
                         {mode === "sign up" && (
-                            <input
-                                type="email"
-                                placeholder="Email address"
-                                className="w-full p-2 mt-2 border-b border-b-gray-300 text-[#6C7275] focus:outline-none text-sm min-[1149px]:text-base"
-                            />
+                            <>
+                                <input
+                                    type="email"
+                                    name='email'
+                                    placeholder="Email address"
+                                    {...register("email", {
+                                        required: "Email is required",
+                                        pattern: { value: /^\S+@\S+$/i, message: "Invalid email address" },
+                                    })}
+                                    className="w-full p-2 mt-2 border-b border-b-gray-300 text-[#6C7275] focus:outline-none text-sm min-[1149px]:text-base"
+                                />
+                                {errors.email && (
+                                    <p className="text-red-500 text-xs">{errors.email.message}</p>
+                                )}
+                            </>
                         )}
 
                         <div className="password-field-container relative w-full">
                             <input
-                                type="password"
+                                type={isVisible ? "text" : "password"}
+                                name='password'
                                 placeholder="Password"
+                                {...register("password", {
+                                    required: "Password is required",
+                                    pattern: {
+                                        value: /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>/?`~\-])(?!.*\s)[A-Za-z\d!@#$%^&*()_+\[\]{};':"\\|,.<>/?`~\-]{4,}$/,
+                                        message: "Password must have 1 uppercase letter, 1 number & min 4 chars"
+                                    }
+
+                                })}
                                 className="w-full p-2 mt-2 border-b border-b-gray-300 text-[#6C7275] focus:outline-none text-sm min-[1149px]:text-base"
                             />
-                            <i className="fa-regular fa-eye absolute right-3 top-[43%] cursor-pointer text-[#6C7275] transition-transform duration-300 hover:scale-110"></i>
+                            {errors.password && (
+                                <p className='text-red-500 text-xs'>{errors.password.message}</p>
+                            )}
+
+                            {/* eye icon */}
+                            {isVisible ? (<i onClick={handleIsVisible}
+                                className="fa-regular fa-eye absolute right-3 top-[43%] cursor-pointer text-[#6C7275] transition-transform duration-300 hover:scale-110"></i>) : (<i onClick={handleIsVisible} className="fa-regular fa-eye-slash absolute right-3 top-[43%] cursor-pointer text-[#6C7275] transition-transform duration-300 hover:scale-110"></i>)}
                         </div>
 
                         {mode === "sign up" && (
-                            <div className="terms flex gap-[12px] items-center">
-                                <input
-                                    type="checkbox"
-                                    className="h-[20px] w-[20px] min-[1149px]:h-[24px] min-[1149px]:w-[24px] cursor-pointer transition-transform duration-300 hover:scale-110"
-                                />
-                                <p className="text-sm min-[1149px]:text-base">
-                                    I agree with <b>Privacy Policy</b> and <b>Terms of Use</b>
-                                </p>
-                            </div>
+                            <>
+                                <div className="terms flex gap-[12px] items-center">
+                                    <input
+                                        type="checkbox"
+                                        {...register("terms", {
+                                            required: "You must accept terms"
+                                        })}
+                                        className="h-[20px] w-[20px] min-[1149px]:h-[24px] min-[1149px]:w-[24px] cursor-pointer transition-transform duration-300 hover:scale-110"
+                                    />
+                                    <label className="text-sm min-[1149x]:text-base">
+                                        I agree with <b>Privacy Policy</b> and <b>Terms of Use</b>
+                                    </label>
+                                </div>
+                                {errors.terms && (
+                                    <p className='text-red-500 text-xs'>{errors.terms.message}</p>
+                                )}
+                            </>
                         )}
 
-                        <button className="bg-[#141718] h-[40px] min-[1149px]:h-[48px] rounded-[8px] font-inter text-white cursor-pointer text-sm min-[1149px]:text-base transition-all duration-300 hover:scale-105 hover:shadow-lg">
+                        {/* sign up or sign in button */}
+                        <button
+                            type='submit'
+                            className="bg-[#141718] h-[40px] min-[1149px]:h-[48px] rounded-[8px] font-inter text-white cursor-pointer text-sm min-[1149px]:text-base transition-all duration-300 hover:scale-105 hover:shadow-lg">
                             {mode === "sign up" ? "Sign Up" : "Sign In"}
                         </button>
-                    </motion.div>
+                    </motion.form>
                 </AnimatePresence>
             </div>
         </div>
